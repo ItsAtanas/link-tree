ecf72c083c7eaf2a704e32eb1674f5c474ff5306
ItsAtanas <bogdanoskiatanas@gmail.com>
2023-04-08 03:06:41 -0400

    description

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,7 @@ export default function Home() {
     <>
       <Head>
         <title>Link Tree</title>
-        <meta
-          name="description"
-          content="App the makes sure that Tim gets on in time"
-        />
+        <meta name="description" content="My Link Tree" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
